21bd76a4a263d7f1ed330dc553c415b3631de7ae
root <michael.henkel@gmail.com>
2015-10-08 12:08:45 +0200

    added content

--- Dockerfiles/keystone/os.sh
+++ Dockerfiles/keystone/os.sh
@@ -1,6 +1,6 @@
 export OS_USERNAME=admin
-export OS_PASSWORD=ladakh1
+export OS_PASSWORD=password
 export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://10.0.0.254:5000/v2.0
-export OS_SERVICE_TOKEN=ladakh1
+export OS_SERVICE_TOKEN=password
 export OS_SERVICE_ENDPOINT=http://10.0.0.254:35357/v2.0
